3b39141a33d9aef216044e71162bd175268e7953
Sirm23333 <sunyuping2018@163.com>
2020-09-28 12:55:07 +0800

    primary:剑指 Offer 25. 合并两个排序的链表

---
+++ src/primary/Offer_25.java
@@ -0,0 +1,46 @@
+package primary;
+
+/**
+ * 剑指 Offer 25. 合并两个排序的链表
+ *
+ * 输入两个递增排序的链表，合并这两个链表并使新链表中的节点仍然是递增排序的。
+ *
+ * 示例1：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ * 限制：
+ *
+ * 0 <= 链表长度 <= 1000
+ */
+public class Offer_25 {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        ListNode newListNode = new ListNode(-1);
+        ListNode p = newListNode;
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                p.next = l1;
+                l1 = l1.next;
+            }else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+        if(l1 != null)
+            p.next = l1;
+        if(l2 != null)
+            p.next = l2;
+        return newListNode.next;
+    }
+}
