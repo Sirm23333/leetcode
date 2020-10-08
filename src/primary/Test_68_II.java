86f129f5c6772527f4b83c2248acd921956b74ab
Sirm23333 <sunyuping2018@163.com>
2020-10-08 15:23:18 +0800

    primary:剑指 Offer 68 - II. 二叉树的最近公共祖先(写法二)

--- src/primary/Test_68_II.java
+++ src/primary/Test_68_II.java
@@ -62,5 +62,19 @@ public class Test_68_II {
         stack.pop();
     }
 
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null)
+            return null;
+        if(root == p || root == q)
+            return root;
+        TreeNode left = lowestCommonAncestor2(root.left , p ,  q);
+        TreeNode right = lowestCommonAncestor2(root.right,p , q);
+        if(left == null && right == null)
+            return null;
+        if(left != null && right != null)
+            return root;
+        return left == null ? right : left;
+
+    }
 
 }
