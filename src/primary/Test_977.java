88cada7135e779b2bdc64a8d428e0041b336494c
Sirm23333 <sunyuping2018@163.com>
2020-10-16 11:53:25 +0800

    primary:977. 有序数组的平方

---
+++ src/primary/Test_977.java
@@ -0,0 +1,17 @@
+package primary;
+
+import java.util.Arrays;
+
+/**
+ *977. 有序数组的平方
+ *
+ */
+public class Test_977 {
+    public int[] sortedSquares(int[] A) {
+        for(int i = 0; i < A.length; i++){
+            A[i] = A[i] * A[i];
+        }
+        Arrays.sort(A);
+        return A;
+    }
+}
